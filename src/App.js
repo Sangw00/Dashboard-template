493bb34652337c400f8d74c01b3a341ef1dd1dbb
sara <sara@iMac-de-YNS.local>
2024-03-26 12:51:43 +0000

    first side and the user in the second part

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import FirstSide from "./Components/FirstSide";
+import SecondSide from "./Components/SecondSide";
 
 function App() {
   return (
     <div className="App">
-    </div>
+      <div className="flex">
+    <FirstSide/>
+    <SecondSide/>
+
+<div className="flex-3">
+
+      <div className="bg-stone-500 pl-0">3</div>
+</div>
+<div className="flex-4">
+      <div className="bg-stone-400 ">4</div>
+</div>
+      </div>
+      </div>
   );
 }
 
---
+++ src/Components/SecondSide.js
@@ -0,0 +1,23 @@
+import { Button } from 'primereact/button';
+
+export default function SecondSide(){
+    
+    return (
+        <div className=" bg-gray-100 flex-2 flex flex-col  overflow-hidden md:w-72">
+         <div class="mt-12 ml-2 flex gap-4 items-center mb-20">
+    <img class="h-14 w-14 rounded-full border-4 border-gray-400 shadow-md shadow-bottom" src="https://pbs.twimg.com/profile_images/1599029039297077249/p0znhFdE_400x400.jpg" alt="" />
+     <div>
+          <h3 class="text-lg font-medium text-gray-700 dark:text-white">AR Shakir</h3>
+          <span class="text-sm tracking-wide text-gray-400 dark:text-gray-300">joined 6 month ago</span>
+        </div>
+      </div>
+      <ul className="flex mr-0 flex-col py-4 mt-4 text-slate-600">
+          
+ </ul>
+      </div>
+    
+
+      
+
+    )
+}
